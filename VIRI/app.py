bc5f1d0a83ef5bb8ab767719d189a5368eeb3a22
Ignat <maksymhladunets@gmail.com>
2025-02-21 14:18:31 +0200

    Дороблено прототип адмін панелі
    * Дані про адміна зберігаються в .env файлі (username, hashed_password, secret_key)
    * Дані зчитуються з .env файлу в app.py
    * На сторінці логіна перевіряється, чи користувач ввів правильні Дані
    * Переадресація на сторінку адміна, якщо дані правильні

--- VIRI/app.py
+++ VIRI/app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template, redirect, url_for, session, request
 import os
-from dotenv import load_dotenv
+from dotenv import load_dotenv, dotenv_values
 import bcrypt
-load_dotenv()
-print(os.environ)
+
+load_dotenv(dotenv_path="VIRI\\.gitignore\\admin.env")
+
 app = Flask(__name__)
+
+app.secret_key = os.getenv("SECRET_KEY")
 
 @app.route("/")
 def home():
@@ -21,21 +24,18 @@ def admin():
 def is_admin(username, password = ""):
     
     ADMIN_USERNAME = os.getenv("ADMIN_USERNAME")
-    ADMIN_PASSWORD = os.getenv(".gitignore/ADMIN_PASSWORD_HASH")
-    print(ADMIN_USERNAME)
-    # print(bcrypt.checkpw(password.encode(), ADMIN_PASSWORD.encode()))
-    return username == ADMIN_USERNAME 
-# and bcrypt.checkpw(password.encode(), ADMIN_PASSWORD.encode())
+    ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD_HASH")
+
+    return username == ADMIN_USERNAME and bcrypt.checkpw(password.encode(), ADMIN_PASSWORD.encode())
 
 @app.route('/login', methods = ["GET", "POST"])
 def login():
     if request.method == "POST":
         adminUsername = request.form.get('username')
         adminPassword = request.form.get("password")
-        # print(adminUsername)
-        # print(adminPassword)
-        # print(is_admin(adminUsername, adminPassword))
+        
         if is_admin(adminUsername, adminPassword):
+            session["admin"] = [adminUsername, adminPassword]
             return redirect(url_for("admin"))
 
     return render_template('login.html')
